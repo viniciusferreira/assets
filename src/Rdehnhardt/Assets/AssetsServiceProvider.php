ed7001a4530f8439dc9102de972b6a0eb9fce0d3
Renato Dehnhardt <renatotkd@gmail.com>
2014-09-14 15:27:47 -0300

    First Service Provider

--- src/Rdehnhardt/Assets/AssetsServiceProvider.php
+++ src/Rdehnhardt/Assets/AssetsServiceProvider.php
@@ -1,34 +1,69 @@
 <?php namespace Rdehnhardt\Assets;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\AliasLoader;
 
-class AssetsServiceProvider extends ServiceProvider {
+class AssetsServiceProvider extends ServiceProvider
+{
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		//
-	}
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('rdehnhardt/assets');
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array();
-	}
+        $this->app->booting(
+            function () {
+                $loader = AliasLoader::getInstance();
+                $loader->alias('Assets', 'Rdehnhardt\Assets\Facades\Assets');
+            }
+        );
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    public function registerAssets()
+    {
+        $this->app->bind(
+            'Assets',
+            function ($app) {
+                return new Assets;
+            }
+        );
+
+        $this->app['assets'] = $this->app->share(
+            function ($app) {
+                return new Assets;
+            }
+        );
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array();
+    }
 
 }
