4560546cfaa50b306ea49d615df3b22927f512ae
Renato Dehnhardt <renatotkd@gmail.com>
2014-09-14 19:59:00 -0300

    FIX Minify Command for all file type!

--- app/Commander/Minify/MinifyCommandHandler.php
+++ app/Commander/Minify/MinifyCommandHandler.php
@@ -8,12 +8,14 @@ class MinifyCommandHandler implements CommandHandler
 {
     public function handle($command)
     {
+        $content = $command->getContent();
+
         switch ($command->getType()) {
             case 'js':
-                $content = JSMin::minify($command->getContent());
+                $content = JSMin::minify($content);
                 break;
             case 'css':
-                $Buffer = new CssMinifier($command->getContent());
+                $Buffer = new CssMinifier($content);
                 $content = $Buffer->getMinified();
                 break;
         }
