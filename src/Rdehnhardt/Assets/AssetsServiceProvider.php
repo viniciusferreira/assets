07e89ae8425750af3f6b19940b28bb6f8a49785d
Renato Dehnhardt <renatotkd@gmail.com>
2014-09-14 15:14:00 -0300

    First Commit

---
+++ src/Rdehnhardt/Assets/AssetsServiceProvider.php
@@ -0,0 +1,34 @@
+<?php namespace Rdehnhardt\Assets;
+
+use Illuminate\Support\ServiceProvider;
+
+class AssetsServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
